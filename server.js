0aeb0648777855ad187de163e56735623087cf67
Arthur Melkonyan <arthur@artmelkon.me>
2021-10-30 19:37:28 +0300

    update

--- server.js
+++ server.js
@@ -41,6 +41,14 @@ app.use(async (req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const startServer = async function() {
   const server = new ApolloServer({
     schema,
--- utils/db_connect.js
+++ utils/db_connect.js
@@ -1,14 +1,6 @@
-const path = require('path')
-const mongoose = require('mongoose');
-require('dotenv').config({ path: './env/.env'});
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./env/.env" });
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
 const PORT = process.env.PORT || 4444;
 
 module.exports = async (app) => {
@@ -24,4 +16,4 @@ module.exports = async (app) => {
       console.log("DB Connected");
     })
     .catch((err) => console.error(err));
-}
+};
